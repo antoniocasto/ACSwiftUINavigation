db119515846584bccde32ee2febdbc6bfaa0f66e
Antonio Casto <mail.antoniocasto@gmail.com>
2025-08-19 21:41:58 +0200

    Associate input payload to AppRoute.

--- Sources/ACSwiftUINavigation/Common/Entities/AppRoute.swift
+++ Sources/ACSwiftUINavigation/Common/Entities/AppRoute.swift
@@ -8,21 +8,9 @@
 import SwiftUI
 
 
-/// A protocol that defines a navigation route within an application.
-/// 
-/// Conforming types represent a destination that can be presented or pushed within a navigation stack,
-/// providing a SwiftUI view and presentation style. Routes are expected to be uniquely identifiable and hashable,
-/// allowing them to be managed in navigation collections.
-/// 
-/// - Requires:
-///    - Conformance to `Identifiable` and `Hashable` for proper collection management and identity.
-///    - A property indicating the presentation style (e.g., push, modal).
-///    - A method for constructing the destination view wrapped in `AnyView`.
-/// 
-/// - Note: This protocol leverages SwiftUI's `@ViewBuilder` for view construction, enabling the composition
-///         of complex views as navigation destinations. Platform availability for SwiftUI types may be required.
-///
 public protocol AppRoute: Identifiable, Hashable {
+    associatedtype InputPayload
+    
     //MARK: - Properties
     
     var presentationStyle: PresentationStyle { get }
